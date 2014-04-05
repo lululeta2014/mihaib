3ce12c9b4d546ee13b0e004b5c1e2b249e7e1b82
mihaib <mihaib@hermes>
2014-04-05 15:58:42 +0300

    update java version

--- lethe/linux-setup/programs/java.sh
+++ lethe/linux-setup/programs/java.sh
@@ -11,16 +11,20 @@ source "$SCRIPT_DIR/../sourceme.bash"
 
 echo '--- Java'
 
-SRV_DIR=7u51-b13
-LATEST=jdk-7u51-linux-x64.tar.gz
-PATTERN='jdk-7*.tar.gz'
+SRV_DIR=8-b132
+LATEST=jdk-8-linux-x64.tar.gz
+PATTERN='jdk-8*.tar.gz'
 download-kit \
 	--delete-pattern "$PATTERN" \
 	--url http://download.oracle.com/otn-pub/java/jdk/"$SRV_DIR"/"$LATEST" \
 	--file "$MB_KITS_DIR"/"$LATEST" \
-	-- --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com"
+	-- --header "Cookie: oraclelicense=accept-securebackup-cookie"
+
+# The cookie used to be:
+#	-- --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com"
+# which still exists, but now only ‘oraclelicense’ is the one needed.
 
 JAVA_DIR="$MB_PRG_DIR"/jdk
 rm -rf "$JAVA_DIR"
 tar -C "$MB_PRG_DIR" -xzf "$MB_KITS_DIR"/$PATTERN
-mv "$MB_PRG_DIR"/jdk1.7.* "$JAVA_DIR"
+mv "$MB_PRG_DIR"/jdk1.8.* "$JAVA_DIR"
