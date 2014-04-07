29183b9f2afb0c23b45589eda240565e931cf947
mihaib <mihaib@hermes>
2014-04-07 14:09:23 +0300

    restrict gnome-system-monitor to Debian Jessie, ubuntu showed an error

--- lethe/linux-setup/common-gui/gnome-system-monitor.sh
+++ lethe/linux-setup/common-gui/gnome-system-monitor.sh
@@ -4,7 +4,20 @@ set -u	# exit if using uninitialised variable
 set -e	# exit if some command in this script fails
 trap "echo $0 failed because a command in the script failed" ERR
 
+SCRIPT=`readlink -f "$0"`
+DIR=`dirname "$SCRIPT"`
+SCRIPTS_SRC=`dirname "$DIR"`/scripts
+
+source "$DIR/../sourceme.bash"
+
 
 # Set the ‘last used tab’ to be ‘resources’ instead of the default ‘disks’,
 # so even the first time we run the program we see the tab we want.
-gsettings set org.gnome.gnome-system-monitor current-tab '"resources"'
+if [ "$MB_LSB_ID" == 'Debian' ]; then
+	if [ "$MB_LSB_CN" == 'jessie' ]; then
+		gsettings set org.gnome.gnome-system-monitor current-tab '"resources"'
+	fi
+else
+	# Ubuntu 13.10 gives an error (the key wants an int).
+	true
+fi
