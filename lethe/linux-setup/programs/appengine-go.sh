93530ca12893fa3ef2244d51e01d6da0519cb5a7
mihaib <mihaib@hermes>
2014-05-31 01:19:35 +0300

    update appengine go

--- lethe/linux-setup/programs/appengine-go.sh
+++ lethe/linux-setup/programs/appengine-go.sh
@@ -11,7 +11,7 @@ source "$SCRIPT_DIR/../sourceme.bash"
 
 echo '--- Google App Engine (Go)'
 
-LATEST=go_appengine_sdk_linux_amd64-1.9.4.zip
+LATEST=go_appengine_sdk_linux_amd64-1.9.5.zip
 PATTERN=go_appengine_sdk_linux_amd64-*.zip
 download-kit \
 	--file "$MB_KITS_DIR"/"$LATEST" \
