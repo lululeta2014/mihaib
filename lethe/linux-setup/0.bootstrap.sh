8a10862cc7e861fbfd33e6c54ab76249914ebc1b
mihaiftr <mihaiftr@amy>
2014-07-25 17:22:33 +0300

    minor: add rabbitmq to path

--- lethe/linux-setup/0.bootstrap.sh
+++ lethe/linux-setup/0.bootstrap.sh
@@ -41,6 +41,7 @@ echo >>~/.profile "
 add_to_path_end /sbin	# useful on Debian, e.g. /sbin/ifconfig
 add_to_path_end $MB_PRG_DIR/chromedriver
 add_to_path_end $MB_PRG_DIR/texttest/source/bin
+add_to_path_start $MB_PRG_DIR/rabbitmq/sbin
 # postgresql must be at start of path because some programs (e.g. psycopg2)
 # use pg_config to find out your installed version and you might already have
 # a system-wide one available.
